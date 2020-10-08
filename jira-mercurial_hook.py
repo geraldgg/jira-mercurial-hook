3da8e6d5a4fe15c01337adc0f7a3acb3841d3c16
gfauvelle <gfauvelle@miteksystems.com>
2020-10-08 17:27:36 +0200

    update jira_updater.py :
      - manage errors
      - manage hours to update working time
    
    jira-mercurial_hook.py :
      - add documentation
      - fix config reading default values
      - fix binary/non binary strings
      - manage hours, fix regex

--- jira-mercurial_hook.py
+++ jira-mercurial_hook.py
@@ -6,7 +6,107 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2 or any later version.
 
-import sys, traceback, re
+'''hooks for integrating with Jira
+
+This hook extension adds comments on issues in Jira when changesets
+that refer to issue by Jira ID are seen. The comment is formatted using
+the Mercurial template mechanism.
+
+The bug references can optionally include an update for jira of the
+hours spent working on the bug. Bugs can also be marked fixed.
+
+Access is done via the atlassian python api (REST-API) and requires
+a jira username and api key specified in the configuration. Comments
+are made under the given username or the user associated with the apikey in jira.
+
+Configuration items common to all access modes:
+
+jira.regexp
+  Regular expression to match bug IDs for update in changeset commit message.
+  It must contain one "()" named group ``<ids>`` containing the bug
+  IDs separated by non-digit characters. It may also contain
+  a named group ``<hours>`` with a floating-point number giving the
+  hours worked on the bug. If no named groups are present, the first
+  "()" group is assumed to contain the bug IDs, second group is assumned to
+  contain hours. The default expression matches ``jira ABCD-1234``,
+  ``jira no. ABCD-1234``, ``jira number ABCD-1234``, ``jira ABCD-1234,5678``,
+  ``jira ABCD-1234 and ABCD-5678`` and variations thereof, followed by an hours
+   number prefixed by ``h`` or ``hours``, e.g. ``hours 1.5``.
+   Matching is case insensitive.
+
+jira.fixregexp
+  Same as previous regexp, except that sjira is searched insteaod of jira
+
+jira.fixstatus
+  The status to set a bug to when marking fixed. Default ``Accepted``.
+
+jira.fixresolution
+  The resolution to set a bug to when marking fixed. Default ``Resolved``.
+
+jira.template
+  Template to use when formatting comments. Overrides style if
+  specified. In addition to the usual Mercurial keywords, the
+  extension specifies:
+
+  :``{bug}``:     The jira bug ID.
+  :``{root}``:    The full pathname of the Mercurial repository.
+  :``{webroot}``: Stripped pathname of the Mercurial repository.
+  :``{hgweb}``:   Base URL for browsing Mercurial repositories.
+
+  Default '{desc|escape}\n\n'
+          '------\n'
+          'Author : {author}\n'
+          'Changeset : {hgweb}/{webroot}/rev/{node|short}\n'
+          'Branch : {branch}'
+
+jira.strip
+  The number of path separator characters to strip from the front of
+  the Mercurial repository path (``{root}`` in templates) to produce
+  ``{webroot}``. For example, a repository with ``{root}``
+  ``/var/local/my-project`` with a strip of 2 gives a value for
+  ``{webroot}`` of ``my-project``. Default 0.
+
+web.baseurl
+  Base URL for browsing Mercurial repositories. Referenced from
+  templates as ``{hgweb}``.
+
+jira.url
+  The base URL for the jira installation.
+  Default ````.
+
+jira.possibleprojects
+  List of possible projects in Jira, separated by a comma. If an issue id
+  is in a project not in this list, it won't be updated
+  example: ABCD,EFGH
+  Default ````.
+
+jira.useremail
+  The user email to use to log into jira.
+
+jira.apikey
+  An apikey generated on the jira instance for api access.
+  Using an apikey removes the need to store the user and password
+  options.
+
+Activating the extension::
+
+    [extensions]
+    jira =
+
+    [hooks]
+    # run jira hook on every change pulled or pushed in here
+    incoming.jira = python:hgext.jira.hook
+
+    [jira]
+    url=https://myjira.atlassian.net/
+    useremail=user@company.com
+    apikey=123456798ABCDEF
+    strip=5
+    template = {desc|escape}\n\n------\nAuthor : {author}\nChangeset : {hgweb}/{webroot}/rev/{node|short}\nBranch : {branch}
+
+'''
+
+import sys, traceback, re, os
 
 sys.path.append("/usr/lib/python3.8/dist-packages")
 sys.path.append('/usr/lib/python38.zip')
@@ -24,6 +124,7 @@ try:
     from mercurial.node import short
     from mercurial import (
         cmdutil,
+        configitems,
         error,
         mail,
         registrar,
@@ -56,15 +157,14 @@ configitem('jira', 'useremail',
            default=None,
            )
 configitem('jira', 'regexp',
-           default=(r'bugs?\s*,?\s*(?:#|nos?\.?|num(?:ber)?s?)?\s*'
-                    r'(?P<ids>(?:\d+\s*(?:,?\s*(?:and)?)?\s*)+)'
-                    r'\.?\s*(?:h(?:ours?)?\s*(?P<hours>\d*(?:\.\d+)?))?')
+           default=('jira?\s*'
+                    '(?P<ids>(?:#?[a-zA-Z]+\-\d+)?)'
+                    '\s*\.?\,?\s*(?:h(?:ours?)?\s*(?P<hours>\d*(?:\.\d+)?))?')
            )
 configitem('jira', 'fixregexp',
-           default=(r'fix(?:es)?\s*(?:bugs?\s*)?,?\s*'
-                    r'(?:nos?\.?|num(?:ber)?s?)?\s*'
-                    r'(?P<ids>(?:#?\d+\s*(?:,?\s*(?:and)?)?\s*)+)'
-                    r'\.?\s*(?:h(?:ours?)?\s*(?P<hours>\d*(?:\.\d+)?))?')
+           default=('sjira\s*'
+                    '(?P<ids>(?:#?[a-zA-Z]+\-\d+)?)'
+                    '\s*\.?\,?\s*(?:h(?:ours?)?\s*(?P<hours>\d*(?:\.\d+)?))?')
            )
 configitem('jira', 'fixresolution',
            default='Done',
@@ -81,7 +181,12 @@ configitem('jira', 'strip',
 configitem('jira', 'template',
            default=None,
            )
+configitem('jira', 'possibleprojects',
+           default=None,
+           )
 
+def config(ui, key):
+    return _(ui.config(_('jira'), _(key), configtable['jira'][key].default))
 
 class jiraaccess(object):
     '''Base class for access to jira.'''
@@ -89,22 +194,17 @@ class jiraaccess(object):
     def __init__(self, ui, repo):
         self.ui = ui
         self.repo = repo
-        url = self.ui.config(_('jira'), _('url'))
-        useremail = self.ui.config(_('jira'), _('useremail'))
-        apikey = self.ui.config(_('jira'), _('apikey'))
+        url = config(ui, 'url')
+        useremail = config(ui, 'useremail')
+        apikey = config(ui, 'apikey')
 
-        default_bug_re = (_('jira?\s*'
-                            '(?P<ids>(?:#?[a-zA-Z]+\-\d+)?)'
-                            '\.?\s*(?:h(?:ours?)?\s*(?P<hours>\d*(?:\.\d+)?))?'))
-
-        default_sbug_re = (_('sjira\s*'
-                             '(?P<ids>(?:#?[a-zA-Z]+\-\d+)?)'
-                             '\.?\s*(?:h(?:ours?)?\s*(?P<hours>\d*(?:\.\d+)?))?'))
-
-        self.bug_re = re.compile(self.ui.config(_('jira'), _('regexp'), default_bug_re), re.IGNORECASE)
-        self.fix_re = re.compile(self.ui.config(_('jira'), _('fixregexp'), default_sbug_re), re.IGNORECASE)
+        self.bug_re = re.compile(config(ui, 'regexp'), re.IGNORECASE)
+        self.fix_re = re.compile(config(ui, 'fixregexp'), re.IGNORECASE)
 
         self.jira = JiraUpdater(useremail.decode('utf-8'), apikey.decode('utf-8'), url.decode('utf-8'))
+        possibleprojects = config(ui, 'possibleprojects')
+        for proj in possibleprojects.split(_(",")):
+            self.jira.add_possible_project(proj.decode('utf-8'))
 
     def find_bugs(self, ctx):
         '''return bugs dictionary created from commit comment.
@@ -116,10 +216,11 @@ class jiraaccess(object):
         start = 0
         hours = 0.0
         bugs = {}
-        bugmatch = self.bug_re.search(ctx.description(), start)
-        fixmatch = self.fix_re.search(ctx.description(), start)
-        self.ui.status(_("ctx=%s\n" % str(ctx.description())))
-        self.ui.status(_("bugmatch=%s, fixmatch=%s\n" % (str(bugmatch), str(fixmatch))))
+        description = ctx.description()
+        self.ui.debug(_("description=%s\n" % description))
+        bugmatch = self.bug_re.search(description, start)
+        fixmatch = self.fix_re.search(description, start)
+        self.ui.debug(_("bugmatch=%s, fixmatch=%s\n" % (str(bugmatch), str(fixmatch))))
 
         while True:
             bugattribs = {}
@@ -136,19 +237,27 @@ class jiraaccess(object):
                     m = fixmatch
             start = m.end()
             if m is bugmatch:
-                bugmatch = self.bug_re.search(ctx.description(), start)
-                if 'fix' in bugattribs:
-                    del bugattribs['fix']
+                bugmatch = self.bug_re.search(description, start)
+                bugattribs['fix'] = False
             else:
-                fixmatch = self.fix_re.search(ctx.description(), start)
-                bugattribs['fix'] = None
+                fixmatch = self.fix_re.search(description, start)
+                bugattribs['fix'] = True
 
+            self.ui.debug(_("groups=%s\n"%str(m.groups())))
             try:
                 ids = m.group('ids')
             except IndexError:
                 ids = m.group(1)
+
             try:
-                hours = float(m.group(_('hours')))
+                bugattribs['hours'] = 0.0
+                try:
+                    hours = m.group('hours')
+                except IndexError:
+                    hours = m.group(2)
+
+                hours = float(hours)
+                self.ui.debug(_("hours: %f\n"%hours))
                 bugattribs['hours'] = hours
             except IndexError:
                 pass
@@ -157,61 +266,24 @@ class jiraaccess(object):
             except ValueError:
                 self.ui.status(_("%s: invalid hours\n") % m.group(_('hours')))
 
-            self.ui.status(_("ids=%s\n" % str(ids)))
+            self.ui.debug(_("ids=%s\n" % str(ids)))
 
             bugs[ids] = bugattribs
         return bugs
 
-    def update(self, bugid, ctx):
-        '''update jira bug with reference to changeset.'''
-
-        def webroot(root):
-            '''strip leading prefix of repo root and turn into
-            url-safe path.'''
-            count = int(self.ui.config(_('jira'), _('strip')))
-            root = util.pconvert(root)
-            while count > 0:
-                c = root.find(_('/'))
-                if c == -1:
-                    break
-                root = root[c + 1:]
-                count -= 1
-            return root
-
-        mapfile = None
-        tmpl = self.ui.config(_('jira'), _('template'))
-        if not tmpl:
-            mapfile = self.ui.config(_('jira'), _('style'))
-        if not mapfile and not tmpl:
-            tmpl = _(
-                '{desc|escape}\n\n------\nAuthor : {author}\nChangeset : {hgweb}/{webroot}/rev/{node|short}\nBranch : {branch}')
-
-        spec = logcmdutil.templatespec(tmpl, mapfile)
-        t = logcmdutil.changesettemplater(self.ui, self.repo, spec, False, None, False)
-        self.ui.pushbuffer()
-        t.show(ctx,
-               changes=ctx.changeset(),
-               bug=bugid,
-               hgweb=self.ui.config(_('web'), _('baseurl')),
-               root=self.repo.root,
-               webroot=webroot(self.repo.root))
-        data = self.ui.popbuffer()
-
-        # Let's modify the data here to keep only the line talking about the bug
-        lines = data.split(_('\n'))
+    def getcomment(self, lines, bugid):
+        comment = ''
         found = False
         end = False
-        newdata = ''
-        self.ui.debug(_('lines=%s\n' % lines))
 
         for line in lines:
-            self.ui.debug(_('search in line %s\n' % str(line)))
+            self.ui.debug(_('search in line %s\n' % line))
             start = 0
             # let's assume we use a template where at the end the changeset is put,after some ---
-            if end or line == _('------'):
+            if end or line == '------':
                 if not end:
-                    newdata += _("\n\n")
-                newdata += line + _("\n")
+                    comment += "\n\n"
+                comment += line + "\n"
                 end = True
             # let's see if we talk about a bug on this line
             while not found:
@@ -227,13 +299,77 @@ class jiraaccess(object):
                 if not id:
                     continue
                 if id == bugid:
-                    newdata = line
+                    comment = line
                     found = True
 
-        self.ui.debug(_('newdata %s\n' % str(newdata)))
+        return comment
 
-        # bugid and newdata are binary, we need to decode to convert to str, jira updater only deals with str
-        self.jira.update_issue(bugid.decode('utf-8'), newdata.decode('utf-8'))
+    def update(self, bug, ctx):
+        '''update jira bug with reference to changeset.'''
+        self.ui.debug(_('\n-------------------\nupdate\n'))
+        bugid, bugattribs = bug
+
+        def webroot(root):
+            '''strip leading prefix of repo root and turn into
+            url-safe path.'''
+            count = int(config(self.ui, 'strip'))
+            root = util.pconvert(root)
+            while count > 0:
+                c = root.find(_('/'))
+                if c == -1:
+                    break
+                root = root[c + 1:]
+                count -= 1
+            return root
+
+        mapfile = None
+        tmpl = config(self.ui, 'template')
+        if not tmpl:
+            mapfile = config(self.ui, 'style')
+        if not mapfile and not tmpl:
+            tmpl = _(
+                '{desc|escape}\n\n'
+                '------\n'
+                'Author : {author}\n'
+                'Changeset : {hgweb}/{webroot}/rev/{node|short}\n'
+                'Branch : {branch}')
+
+        self.ui.debug(_('bug=%s\n'%str(bug)))
+
+        spec = logcmdutil.templatespec(tmpl, mapfile)
+        t = logcmdutil.changesettemplater(self.ui, self.repo, spec, False, None, False)
+        self.ui.pushbuffer()
+        t.show(ctx,
+               changes=ctx.changeset(),
+               bug=bugid,
+               hgweb=self.ui.config(_('web'), _('baseurl')),
+               root=self.repo.root,
+               webroot=webroot(self.repo.root))
+        data = self.ui.popbuffer()
+
+        # Let's modify the data here to keep only the line talking about the bug
+        lines = data.split(_('\n'))
+        comment = self.getcomment(lines, bugid)
+
+        self.ui.debug(_('bug\n  - comment: %s\n' % comment))
+        self.ui.debug(_('  - fix: %s\n' % str(bugattribs['fix'])))
+        self.ui.debug(_('  - hours: %s\n' % str(bugattribs['hours'])))
+
+        try:
+            # bugid and comment are binary, we need to decode to convert to str, jira updater only deals with str
+            if bugattribs['fix']:
+                self.jira.resolve_issue(bugid.decode('utf-8'), comment.decode('utf-8'), bugattribs['hours'])
+            else:
+                self.jira.update_issue(bugid.decode('utf-8'), comment.decode('utf-8'), bugattribs['hours'])
+
+            self.ui.status(_('Issue %s : Updated\n'%(bugid.decode('utf-8'))))
+        except RuntimeError as e:
+            self.ui.warn(_("Issue %s : %s\n"%(bugid.decode('utf-8'), str(e))))
+        except Exception as e:
+            print("Exception:" + str(type(e))+" "+str(e))
+            print('-' * 60)
+            traceback.print_exc(file=sys.stdout)
+            print('-' * 60)
 
 
 def hook(ui, repo, hooktype, node=None, **kwargs):
@@ -246,6 +382,8 @@ def hook(ui, repo, hooktype, node=None, **kwargs):
         print('Not calling jira hook, repo is "' + repo.root + '"')
         return
 
+    configitems.loadconfigtable(ui, "", configtable)
+
     if node is None:
         raise error.Abort(_('hook type %s does not pass a changeset id') % hooktype)
     try:
@@ -257,9 +395,9 @@ def hook(ui, repo, hooktype, node=None, **kwargs):
         ui.debug(_('Found bugs %s\n' % str(bugs)))
 
         if bugs:
-            for bug in bugs:
+            for bug in bugs.items():
                 ja.update(bug, ctx)
 
     except Exception as e:
-        raise error.Abort(_('jira error: %s') % str(e))
+        raise error.Abort(_('jira error: %s' % str(e)))
         pass
--- jira_updater.py
+++ jira_updater.py
@@ -1,23 +1,40 @@
+import urllib3
 from atlassian import Jira
 
 class JiraUpdater:
     def __init__(self, user_email, user_apikey, server):
-        self.jira = Jira(server, username=user_email, password=user_apikey)
+        self.jira = Jira(server, username=user_email, password=user_apikey, advanced_mode=True)
         self.possible_projects = []
 
     def add_possible_project(self, project):
         self.possible_projects.append(project)
 
     def verify_project(self, issue_id):
-        res = self.jira.get_issue(issue_id, 'project')
+        try:
+            res = self.jira.get_issue(issue_id, 'project')
+        except urllib3.exceptions.MaxRetryError as e:
+            raise RuntimeError(str(e))
+
+        if res.status_code == 401:
+            raise RuntimeError("Error: %s. Check your login and apikey.\n%s"%(res.reason, res.text))
+
+        res = res.json()
+        if 'errorMessages' in res:
+            raise RuntimeError("Error : "+res['errorMessages'][0])
+
         project_key = res['fields']['project']['key']
         if not project_key in self.possible_projects:
             raise RuntimeError("Issue %s not in possible projects %s" % (str(issue_id), str(self.possible_projects)))
 
-    def resolve_issue(self, issue_id, comment):
+    def resolve_issue(self, issue_id, comment, hours):
         # 'Ready', 'On Hold', 'In Progress', 'Done'
+        self.verify_project(issue_id)
         self.jira.set_issue_status(issue_id, 'Done')
-        self.jira.issue_add_comment(issue_id, comment)
+        self.update_issue(issue_id, comment, hours)
 
-    def update_issue(self, issue_id, comment):
+    def update_issue(self, issue_id, comment, hours):
+        self.verify_project(issue_id)
         self.jira.issue_add_comment(issue_id, comment)
+        if hours>0.0:
+            self.jira.issue_worklog(issue_id, None, hours*3600.0)
+
